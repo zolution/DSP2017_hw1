9e182a50fa8567f2ca72ad89c568f442663b1abd
zolution <zolution@outlook.com>
2017-10-25 22:00:12 +0800

    Finish calculate beta

--- train.cpp
+++ train.cpp
@@ -14,8 +14,19 @@ double a[1000][1000];
 double b[1000][1000];
 double alpha[1000][1000];
 double beta[1000][1000];
+double eps[1000][1000];
+double gamma[1000][1000];
 int n;
 
+void init(){
+	for(int i=0;i<1000;i++){
+		for(int j=0;j<1000;j++){
+			eps[i][j] = 0;
+			gamma[i][j] = 0;
+		}
+	}
+	return;
+}
 void input_init_model(){
 	char temp[1000];
 	fscanf(t_init_model,"%s%d",temp,&n);
@@ -52,7 +63,19 @@ void calculate_alpha(int *o, int T){
 }
 
 void calculate_beta(int *o, int T){
-	
+	for(int i=0;i<n;i++){
+		beta[T-1][i] = 1.0;
+	}
+	for(int t=T-2;t>=0;t--){
+		for(int i=0;i<n;i++){
+			double sum = 0.0;
+			for(int j=0;j<n;j++){
+				sum += (a[i][j] * b[j][o[t+1]] * beta[t+1][j]);
+			}
+			beta[t][i] = sum;
+		}
+	}
+	return;
 }
 
 void iteration(char *o){
@@ -61,6 +84,8 @@ void iteration(char *o){
 	for(int g=0;g<len;g++) obs[g] = o[g]='A';
 	calculate_alpha(obs, len);
 	calculate_beta(obs, len);
+
+
 }
 
 int main(int argc, char **argv){
@@ -69,7 +94,7 @@ int main(int argc, char **argv){
 	}
 	t_init_model = fopen(argv[1],"r");
 	t_data_input = fopen(argv[2],"r");
-	
+	init();
 	input_init_model();
 
 }
